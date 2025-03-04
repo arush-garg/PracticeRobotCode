ef22b5cc0721ff3500a159d8db6715eb3cdb4000
alaninnovates <alanlichen1@gmail.com>
2025-03-03 21:32:09 -0800

    Add swerve drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,68 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+// import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.Drive.EagleSwerveDrivetrain;
+import frc.robot.subsystems.Drive.EagleSwerveTelemetry;
 
 public class RobotContainer {
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max
+
+  // swerve
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+  private final EagleSwerveTelemetry logger = new EagleSwerveTelemetry(MaxSpeed);
+  public final EagleSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+  // controllers
+  private final CommandJoystick m_leftJoystick = new CommandJoystick(0);
+  private final CommandJoystick m_rightJoystick = new CommandJoystick(1);
+  private final CommandXboxController m_operatorController = new CommandXboxController(2);
+  private final CommandXboxController m_buttonBoard = new CommandXboxController(3);
+
+  // auto
   private final SendableChooser<Command> autoChooser;
 
   public RobotContainer() {
-    autoChooser = AutoBuilder.buildAutoChooser();
+    autoChooser = AutoBuilder.buildAutoChooser("Tests");
     SmartDashboard.putData("Auto Chooser", autoChooser);
+
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    drivetrain.setDefaultCommand(
+        drivetrain.applyRequest(() -> drive.withVelocityX(-m_leftJoystick.getY() * MaxSpeed)
+            .withVelocityY(-m_leftJoystick.getX() * MaxSpeed)
+            .withRotationalRate(-m_rightJoystick.getX() * MaxAngularRate)));
+
+    // todo: sysid routines for tuning
+    // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+    // reset yaw
+    m_operatorController.button(8).onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+    drivetrain.registerTelemetry(logger::telemeterize);
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/vision/Vision.java
+++ src/main/java/frc/robot/vision/Vision.java
@@ -2,17 +2,32 @@ package frc.robot.vision;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.VisionConstants;
+import frc.robot.subsystems.Drive.EagleSwerveDrivetrain;
 
 public class Vision extends SubsystemBase {
     VisionCamera cameraRight, cameraLeft;
+    EagleSwerveDrivetrain drivetrain;
 
-    Vision() {
+    Vision(EagleSwerveDrivetrain drivetrain) {
+        this.drivetrain = drivetrain;
         cameraRight = new VisionCamera(VisionConstants.CAMERA_RIGHT_CONFIG);
         cameraLeft = new VisionCamera(VisionConstants.CAMERA_LEFT_CONFIG);
     }
 
     @Override
     public void periodic() {
-        // update drivebase odom
+        updateDrivetrainVision(cameraRight);
+        updateDrivetrainVision(cameraLeft);
+    }
+
+    private void updateDrivetrainVision(VisionCamera camera) {
+        var visionEst = camera.getEstimatedGlobalPose();
+        visionEst.ifPresent(
+                est -> {
+                    var estStdDevs = camera.getEstimationStdDevs();
+
+                    drivetrain.addVisionMeasurement(
+                            est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
+                });
     }
 }
