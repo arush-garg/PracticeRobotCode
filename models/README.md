c7a9dda5266f453fb6dc7ad09d44941049518045
alaninnovates <alanlichen1@gmail.com>
2025-03-23 23:56:32 -0700

    add mechanism visualizations from cad

---
+++ models/README.md
@@ -0,0 +1,4 @@
+model: robot base
+model_0: intake
+model_1: ee
+model_2: elev carriage
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,15 @@
 package frc.robot;
 
 import au.grapplerobotics.CanBridge;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -8,6 +17,10 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private final RobotContainer m_robotContainer;
+  StructPublisher<Pose2d> zeroedPosePublisher = NetworkTableInstance.getDefault()
+      .getStructTopic("DrivebaseZeroedPose", Pose2d.struct).publish();
+  StructArrayPublisher<Pose3d> mechPosePublisher = NetworkTableInstance.getDefault()
+      .getStructArrayTopic("MechanismPosesZeroed", Pose3d.struct).publish();
 
   public Robot() {
     CanBridge.runTCP();
@@ -17,6 +30,12 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    zeroedPosePublisher.set(new Pose2d(new Translation2d(0, 0), new Rotation2d(0)));
+    mechPosePublisher.set(new Pose3d[] {
+        new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0)),
+        new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0)),
+        new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0))
+    });
   }
 
   @Override
