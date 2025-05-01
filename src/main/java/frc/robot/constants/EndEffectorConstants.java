050a0bebac07057a691d4c112ccab1adf388fd4f
alaninnovates <alanlichen1@gmail.com>
2025-05-01 01:11:13 -0700

    dealgae cmd

--- src/main/java/frc/robot/constants/EndEffectorConstants.java
+++ src/main/java/frc/robot/constants/EndEffectorConstants.java
@@ -41,6 +41,7 @@ public final class EndEffectorConstants {
         public final static double RETAIN_ALGAE = 2.0;
         public final static double RETAIN_CORAL = 1.0;
         public final static double EJECT_VOLTS = -5;
+        public final static double DEALGAE_VOLTS = -5;
 
         public final static double STALL_CURRENT = 30;
 
